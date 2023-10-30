28f0cd0cea23f3670c0b5927fce5b20eb000a5fe
CasDDDD <lhagwaochir69@gmail.com>
2023-10-30 20:12:22 +0800

    10/30/24

--- todo/index.js
+++ todo/index.js
@@ -1,4 +1,13 @@
-let addBtns = document.getElementById("01");
-function hoverbtn() {
-    onclick.
-}
+let one = document.getElementById("one");
+let addTask = document.getElementById("two");
+
+one.onclick = function () {
+  addTask.style.display = "block";
+  addTask.style.display = "flex";
+  addTask.style.justifyContent = "spacebetween";
+};
+window.onclick = function (event) {
+  if (event.target === addTask) {
+    addTask.style.display = "none";
+  }
+};
