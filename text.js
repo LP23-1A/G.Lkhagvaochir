82b85ef010fad1ccf5a65eacd9c996ec4822d855
CasDDDD <lhagwaochir69@gmail.com>
2023-11-02 12:32:13 +0800

    uurchlult

---
+++ text.js
@@ -0,0 +1,6 @@
+function print(hewleh) {
+  return "hi " + hewleh;
+}
+console.log(print("hewleh"));
+console.log(print("hoyr"));
+console.log(print("mongol"));
--- todo/index.js
+++ todo/index.js
@@ -21,26 +21,30 @@ addBtn.onclick = addTask;
 
 overlay.onclick = closeModal;
 
-let title = titleInput.addEventListener("change", (event) => {
-  return event.target.value;
-});
+const data = [
+  {
+    title: "todo",
+    desc: "this a todo card",
+    status: "done",
+    priority: "high",
+  },
+];
 
-const cardElement = `<div class="card flex gap12">
+function addTask(cards) {
+  let done = document.querySelectorAll(".cards");
+  const { title, desc, priority } = cards;
+  return `<div class="card flex gap12">
               <img class="done crs" src="./icons/check-mark.png" alt="" />
               <div class="details flex1 gap12 flex column">
-                <h4>Todo</h4>
-                <p>this is a todo card</p>
-                <div class="Priority">high</div>
+                <h4>${title}</h4>
+                <p>${desc}</p>
+                <div class="priority">${priority}}</div>
               </div>
               <div class="actions flex column gap8 crs">
-                <img class="done" src="./icons/x-mark.png" alt="" />
-                <img class="done" src="./icons/edit.png" alt="" />
+                <img class="remove" src="./icons/x-mark.png" alt="" />
+                <img class="edit" src="./icons/edit.png" alt="" />
               </div>
             </div>`;
-
-function addTask() {
-  let done = document.querySelector("#done");
-  done.innerHTML += cardElement;
 
   closeModal();
 }
