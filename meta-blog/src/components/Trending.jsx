43bb867b986e5bdaabd47e9683f13c3c89f4eb13
CasDDDD <145735492+CasDDDD@users.noreply.github.com>
2023-12-04 12:37:50 +0800

    implement navbar component (#21)

---
+++ meta-blog/src/components/Trending.jsx
@@ -0,0 +1,15 @@
+const Blog = [
+  {
+    title: "Blog Post Title",
+    description: "This is a sample blog post.",
+    date: "2019-05-30T17:46:08.317Z",
+  },
+];
+export default function Trending() {
+  return (
+    <div>
+      <h1 className="">All Blog post</h1>
+      <div>{data.map((e) => {})}</div>
+    </div>
+  );
+}
