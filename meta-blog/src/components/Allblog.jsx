72049b8c1ebd3f3cf207d7b687a02520a39f5580
CasDDDD <145735492+CasDDDD@users.noreply.github.com>
2023-12-05 12:39:28 +0800

    implemented static components (#23)
    Co-authored-by: Cas <cas@Cass-MacBook-Pro.local>

---
+++ meta-blog/src/components/Allblog.jsx
@@ -0,0 +1,130 @@
+import Blog1 from "@/images/Blog1.png";
+import { v4 as uuidv4 } from "uuid";
+
+const data = [
+  {
+    id: uuidv4(),
+    img: Blog1,
+    title: "Technology",
+    description:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    date: "August 20, 2022",
+  },
+  {
+    id: uuidv4(),
+    img: Blog1,
+    title: "Design",
+    description:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    date: "August 20, 2022",
+  },
+  {
+    id: uuidv4(),
+    img: Blog1,
+    title: "Technology",
+    description:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    date: "August 20, 2022",
+  },
+  {
+    id: uuidv4(),
+    img: Blog1,
+    title: "Technology",
+    description:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    date: "August 20, 2022",
+  },
+  {
+    id: uuidv4(),
+    img: Blog1,
+    title: "Technology",
+    description:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    date: "August 20, 2022",
+  },
+  {
+    id: uuidv4(),
+    img: Blog1,
+    title: "Technology",
+    description:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    date: "August 20, 2022",
+  },
+  {
+    id: uuidv4(),
+    img: Blog1,
+    title: "Technology",
+    description:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    date: "August 20, 2022",
+  },
+  {
+    id: uuidv4(),
+    img: Blog1,
+    title: "Technology",
+    description:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    date: "August 20, 2022",
+  },
+  {
+    id: uuidv4(),
+    img: Blog1,
+    title: "Technology",
+    description:
+      "The Impact of Technology on the Workplace: How Technology is Changing",
+    date: "August 20, 2022",
+  },
+];
+export default function AllBlog() {
+  return (
+    <div className="flex flex-col gap-8 max-w-[1230px] m-auto">
+      <div className="flex flex-col gap-8">
+        <h1 className="text-[24px] text-[#181A2A] font-bold">All Blog post</h1>
+        <div className="flex justify-between text-[12px] text-[#495057] font-bold">
+          <div className="gap-5 flex ">
+            <p className="text-[#D4A373]">All</p>
+            <p>Design</p>
+            <p>Travel</p>
+            <p>Fashion</p>
+            <p>Technology</p>
+            <p>Branding</p>
+          </div>
+          <div>
+            <p>View All</p>
+          </div>
+        </div>
+      </div>
+      <div className="gap-5 grid-rows-3 grid-cols-3 grid">
+        {data.map((e) => {
+          return (
+            <div
+              key={e.id}
+              className="flex flex-col rounded-[12px] border-solid border-[#E8E8EA] border-2 p-4 m-auto"
+            >
+              <div className="flex gap-5 flex-col">
+                <img src={e.img.src} alt="" />
+                <div className="flex flex-col ">
+                  <a
+                    href="Technology"
+                    className="rounded-[1.5px] px-[5px] py-1 text-[#4B6BFB] bg-[#f1f1f5] text-center justify-center max-w-[95px] items-center"
+                  >
+                    {e.title}
+                  </a>
+                  <p className="text-[#181A2A] text-[24px] font-semibold">
+                    {e.description}
+                  </p>
+                </div>
+              </div>
+              <div className="text-[#97989F] text-[16px] font-normal">
+                {e.date}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <button className="flex gap-3 items-center justify-center px-5 py-3 text-[16px] font-medium rounded-[12px] border-solid border-[#E8E8EA] border-2 m-auto text-[#696A75]">
+        Load More
+      </button>
+    </div>
+  );
+}
--- meta-blog/src/components/Navbar.jsx
+++ meta-blog/src/components/Navbar.jsx
@@ -2,14 +2,22 @@ import Logo from "@/images/Logo";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between m-auto px-[350px] py-[32px] w-[1920px]">
-      <Logo />
+    <div className="flex items-start py-8 m-auto max-w-[1230px]">
+      <div className="flex items-start">
+        <Logo />
+      </div>
       <div className="flex gap-10 text-[16px] font-normal rounded-[5px] text-[#3B3C4A]">
-        <div>Home</div>
-        <div>BLog</div>
-        <div>Contact</div>
+        <div className="flex gap-10 justify-center items-center w-[667px] text-[#3B3C4A] text-[16px]">
+          <p>Home</p>
+          <p>BLog</p>
+          <p>Contact</p>
+        </div>
         <div className="flex">
-          <input type="search" className="bg-[#F4F4F5] w-[166px]" />
+          <input
+            type="search"
+            placeholder="Search"
+            className="bg-[#F4F4F5] w-[166px] h-[36px] pl-4 px-2"
+          />
         </div>
       </div>
     </div>
