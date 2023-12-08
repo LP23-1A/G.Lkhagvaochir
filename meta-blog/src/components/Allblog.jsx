77f5addf78e8f1135ac3aa9173a16804f8ced3dc
CasDDDD <lhagwaochir69@gmail.com>
2023-12-08 11:56:35 +0800

    process

--- meta-blog/src/components/Allblog.jsx
+++ meta-blog/src/components/Allblog.jsx
@@ -1,84 +1,93 @@
-import Blog1 from "@/images/Blog1.png";
 import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
+import { useState, useEffect } from "react";
 
-const api = "";
+let api = "https://dev.to/api/articles?username=gereltuyamz";
 
-const data = [
-  {
-    id: uuidv4(),
-    img: Blog1,
-    title: "Technology",
-    description:
-      "The Impact of Technology on the Workplace: How Technology is Changing",
-    date: "August 20, 2022",
-  },
-  {
-    id: uuidv4(),
-    img: Blog1,
-    title: "Design",
-    description:
-      "The Impact of Technology on the Workplace: How Technology is Changing",
-    date: "August 20, 2022",
-  },
-  {
-    id: uuidv4(),
-    img: Blog1,
-    title: "Technology",
-    description:
-      "The Impact of Technology on the Workplace: How Technology is Changing",
-    date: "August 20, 2022",
-  },
-  {
-    id: uuidv4(),
-    img: Blog1,
-    title: "Technology",
-    description:
-      "The Impact of Technology on the Workplace: How Technology is Changing",
-    date: "August 20, 2022",
-  },
-  {
-    id: uuidv4(),
-    img: Blog1,
-    title: "Software",
-    description:
-      "The Impact of Technology on the Workplace: How Technology is Changing",
-    date: "August 20, 2022",
-  },
-  {
-    id: uuidv4(),
-    img: Blog1,
-    title: "Technology",
-    description:
-      "The Impact of Technology on the Workplace: How Technology is Changing",
-    date: "August 20, 2022",
-  },
-  {
-    id: uuidv4(),
-    img: Blog1,
-    title: "Technology",
-    description:
-      "The Impact of Technology on the Workplace: How Technology is Changing",
-    date: "August 20, 2022",
-  },
-  {
-    id: uuidv4(),
-    img: Blog1,
-    title: "Technology",
-    description:
-      "The Impact of Technology on the Workplace: How Technology is Changing",
-    date: "August 20, 2022",
-  },
-  {
-    id: uuidv4(),
-    img: Blog1,
-    title: "Technology",
-    description:
-      "The Impact of Technology on the Workplace: How Technology is Changing",
-    date: "August 20, 2022",
-  },
-];
+// const data = [
+//   {
+//     id: uuidv4(),
+//     img: Blog1,
+//     title: "Technology",
+//     description:
+//       "The Impact of Technology on the Workplace: How Technology is Changing",
+//     date: "August 20, 2022",
+//   },
+//   {
+//     id: uuidv4(),
+//     img: Blog1,
+//     title: "Design",
+//     description:
+//       "The Impact of Technology on the Workplace: How Technology is Changing",
+//     date: "August 20, 2022",
+//   },
+//   {
+//     id: uuidv4(),
+//     img: Blog1,
+//     title: "Technology",
+//     description:
+//       "The Impact of Technology on the Workplace: How Technology is Changing",
+//     date: "August 20, 2022",
+//   },
+//   {
+//     id: uuidv4(),
+//     img: Blog1,
+//     title: "Technology",
+//     description:
+//       "The Impact of Technology on the Workplace: How Technology is Changing",
+//     date: "August 20, 2022",
+//   },
+//   {
+//     id: uuidv4(),
+//     img: Blog1,
+//     title: "Software",
+//     description:
+//       "The Impact of Technology on the Workplace: How Technology is Changing",
+//     date: "August 20, 2022",
+//   },
+//   {
+//     id: uuidv4(),
+//     img: Blog1,
+//     title: "Technology",
+//     description:
+//       "The Impact of Technology on the Workplace: How Technology is Changing",
+//     date: "August 20, 2022",
+//   },
+//   {
+//     id: uuidv4(),
+//     img: Blog1,
+//     title: "Technology",
+//     description:
+//       "The Impact of Technology on the Workplace: How Technology is Changing",
+//     date: "August 20, 2022",
+//   },
+//   {
+//     id: uuidv4(),
+//     img: Blog1,
+//     title: "Technology",
+//     description:
+//       "The Impact of Technology on the Workplace: How Technology is Changing",
+//     date: "August 20, 2022",
+//   },
+//   {
+//     id: uuidv4(),
+//     img: Blog1,
+//     title: "Technology",
+//     description:
+//       "The Impact of Technology on the Workplace: How Technology is Changing",
+//     date: "August 20, 2022",
+//   },
+// ];
 export default function AllBlog() {
+  const [data, setData] = useState([]);
+  const getData = async (api) => {
+    let res = await axios.get(api);
+    setData(() => [...res.data]);
+  };
+  useEffect(() => {
+    getData(api);
+  }, []);
+
   return (
     <div className="flex flex-col gap-8 max-w-[1230px] m-auto">
       <div className="flex flex-col gap-8">
@@ -99,27 +108,28 @@ export default function AllBlog() {
       </div>
       <div className="gap-5 grid-rows-3 grid-cols-3 grid">
         {data.map((e) => {
+          let key = uuidv4();
           return (
             <div
-              key={e.id}
-              className="flex flex-col rounded-[12px] border-solid border-[#E8E8EA] border-2 p-4 m-auto"
+              key={key}
+              className="flex flex-col rounded-[12px] border-solid border-[#E8E8EA] border-2 p-4 mx-auto"
             >
               <div className="flex gap-5 flex-col">
-                <img src={e.img.src} alt="" />
+                <img src={e.social_image} alt="" />
                 <div className="flex flex-col ">
                   <a
                     href="Technology"
                     className="rounded-md px-[5px] py-1 text-[#4B6BFB] bg-[#f1f1f5] text-center justify-center max-w-[95px] items-center"
                   >
-                    {e.title}
+                    {e.tags}
                   </a>
                   <p className="text-[#181A2A] text-[24px] font-semibold">
                     {e.description}
                   </p>
                 </div>
               </div>
-              <div className="text-[#97989F] text-[16px] font-normal">
-                {e.date}
+              <div className="text-[#97989F] text-[16px] font-normal ">
+                {e.published_timestamp}
               </div>
             </div>
           );
